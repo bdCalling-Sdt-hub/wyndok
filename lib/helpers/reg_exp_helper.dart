6429763d368394ba946cc341f25452af2885ce99
naimulhassan <developernaimul00@gmail.com>
2024-05-16 22:57:53 +0600

    Book Appointment

--- lib/helpers/reg_exp_helper.dart
+++
@@ -1,8 +0,0 @@
-
-
-
-
-class RegExpHelper {
-  static RegExp emailRegexp = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-  static RegExp passRegExp = RegExp(r'(?=.*[a-z])(?=.*[0-9])');
-}
